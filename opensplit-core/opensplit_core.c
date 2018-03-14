1e777b78021014a7d0d30b123fad5ceab0d403fc
Sapein <chandler182@gmail.com>
2018-03-13 22:33:19 -0400

    Update opensplit_core to match new base64 library
    Update the opensplit_core base64 functions in order to allow it to
    better align with the new base64 library used.

--- opensplit-core/opensplit_core.c
+++ opensplit-core/opensplit_core.c
@@ -3,12 +3,11 @@
 #include <string.h>
 #include "deps/crypto-algorithms/base64.h"
 
-void OpenSplit_Core_Encode_To_Base64(const unsigned char *data, unsigned char *out){
-  /* base64_encode((const unsigned int *)data, strlen((char *)data), out); */
-  ;
+size_t OpenSplit_Core_Encode_To_Base64(const unsigned char *data, unsigned char *out,
+				     size_t data_len, bool newline_flag){
+  return base64_encode(data, out, data_len, newline_flag);
 }
 
-void OpenSplit_Core_Decode_From_Base64(const unsigned char *data, unsigned char *out){
-  /* bas64_decode(data, strlen((char *)data), (unsigned int *)out); */
-  ;
+size_t OpenSplit_Core_Decode_From_Base64(const unsigned char *data, unsigned char *out, size_t data_len){
+  return base64_decode(data, out, data_len);
 }
--- opensplit-core/opensplit_core.h
+++ opensplit-core/opensplit_core.h
@@ -1,5 +1,11 @@
 #ifndef OPENSPLIT_CORE
 #define OPENSPLIT_CORE
-void OpenSplit_Core_Encode_To_Base64(const unsigned char *data, unsigned char *out);
-void OpenSplit_Core_Decode_From_Base64(const unsigned char *data, unsigned char *out);
+
+#include <stdbool.h>
+#include <stddef.h>
+
+size_t OpenSplit_Core_Encode_To_Base64(const unsigned char *data, unsigned char *out,
+				     size_t data_len, bool newline_flag);
+
+size_t OpenSplit_Core_Decode_From_Base64(const unsigned char *data, unsigned char *out, size_t data_len);
 #endif
