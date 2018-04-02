2506cc6be0e1b09548cfceaf960530c31dedc794
Sapein <sapeint@hashbang.sh>
2018-04-02 19:44:37 -0400

    Proper Guards, enum renaming, enum value adding

--- opensplit-core/platform_dependent/linux/general_memory.h
+++ opensplit-core/platform_dependent/linux/general_memory.h
@@ -1,10 +1,12 @@
+#if defined(__linux__) && ((__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC__MINOR__ > 15)))
 #include <unistd.h>
 
 #define ALLOCATION_FAILURE_LIMIT 5
 #define MAX_PID_LOCATION "/proc/sys/kernel/pid_max"
 
-enum Core_Memory_errors {MEMORY_ALLOCATION_FAILURE, PROCESS_NOT_FOUND};
+enum Core_Memory_errors {NO_ERRORS, MEMORY_ALLOCATION_FAILURE, PROCESS_ID_NOT_FOUND};
 extern enum Core_Memory_errors OpenSplit_Core_Memory_Error;
 
 pid_t pidof(char *process_name);
 unsigned long int get_maximum_count(void);
+#endif
